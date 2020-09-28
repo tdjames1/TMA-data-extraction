d7b3641fab66e22c27c0c3462b0bfcdebd665a5d
Tamora James <T.D.James1@leeds.ac.uk>
2020-09-28 11:41:51 +0100

    Add method to create gridded output of weather warning data

--- python/process_content.py
+++ python/process_content.py
@@ -8,10 +8,15 @@ import numpy.linalg as LA
 import bezier
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+from matplotlib.path import Path
 
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import cartopy.io.shapereader as shpreader
+
+import skimage.draw
+import xarray as xr
+import pandas as pd
 
 PDF_GS_OPS = {
     'g': 'setgray (nonstroke)',
@@ -219,18 +224,18 @@ def createPlot(images, contours):
             xy = position
             print("Position:", xy)
             print("Size:", w, "x", h)
-            
+
             pos_check = xy[1] + h < 450
             size_check = w > 120
 
             if pos_check & size_check:
-                rect = mpatches.Rectangle(tuple(xy), w, h, 
+                rect = mpatches.Rectangle(tuple(xy), w, h,
                                           fc="none", ec="green")
                 ax.add_patch(rect)
                 arr_img = plt.imread(MAP_IMG, format='png')
                 ax.imshow(arr_img, interpolation='none',
                           origin='lower',
-                          extent=[xy[0], xy[0]+w, xy[1]+h, xy[1]], 
+                          extent=[xy[0], xy[0]+w, xy[1]+h, xy[1]],
                           clip_on=True)
 
     _ = ax.set_xlim(0, 842)
@@ -261,13 +266,19 @@ def getMapGroups(images, graphics):
             if pos_check & size_check:
                 # Get graphics within map boundary
                 graphics_list = []
+                centroids = []
                 for gfx in graphics:
                     ix, iy = gfx['path']['centroid']
                     #print("Centroid: ", ix, ",", iy)
                     x_check = (x < ix) & (ix < x + w)
                     y_check = (y < iy) & (iy < y + h)
                     if x_check & y_check:
-                        graphics_list.append(gfx)
+                        print("Centroid: ", ix, ",", iy)
+                        if (ix, iy) not in centroids:
+                            graphics_list.append(gfx)
+                            centroids.append((ix, iy))
+                        else:
+                            print("Already added graphics with matching centroid")
                 #print("Got graphics:", len(graphics_list))
                 map_groups.append((img, graphics_list))
 
@@ -277,13 +288,13 @@ def getMapGroups(images, graphics):
             map_groups.sort(key = getXPos)
     return map_groups
 
-def plotMapGroup(map_group, ax):
+def transformMapGroup(map_group):
     img, graphics = map_group
 
     print("Image:", img['name'])
 
     # Construct current transformation matrix for image
-    #   a b 0 
+    #   a b 0
     #   c d 0
     #   e f 1
     m1 = np.hstack((img['ctm'].reshape(3,2), np.array([[0],[0],[1]])))
@@ -301,10 +312,10 @@ def plotMapGroup(map_group, ax):
     # Pre-multiply transformation matrices
     m = np.matmul(m1_inv, m2)
 
-    n_col = len(plt.rcParams['axes.prop_cycle'])
+    graphics_new = []
     print("Processing graphics:", len(graphics))
     for i in range(len(graphics)):
-        col = "C" + str(i%n_col)
+        col = None
         if graphics[i]['colour'] is not None:
             #print("got colour state:", graphics[i]['colour'])
             # Get stroke colour specification
@@ -322,6 +333,7 @@ def plotMapGroup(map_group, ax):
                 col = tuple(col_spec['val']) * 3
             else:
                 col = (0.,0.,0.)
+        contour = []
         for curve in graphics[i]['path']['contour']:
             ## Relocate curve according to new coordinate system
             nodes = curve.nodes
@@ -334,25 +346,231 @@ def plotMapGroup(map_group, ax):
                 v = np.matmul(np.append(nodes.T[i], 1), m)
                 nodes_new.append(v[:-1])
             nodes = np.array(nodes_new).T
-            curve = bezier.Curve.from_nodes(nodes)
+            curve_new = bezier.Curve.from_nodes(nodes)
+            contour.append(curve_new)
+        # Relocate centroid i
+        centroid = np.matmul(np.append(graphics[i]['path']['centroid'], 1), m)[:-1]
+        path = { 'colour': col,
+                 'contour': contour,
+                 'centroid': centroid }
+        graphics_new.append({ 'path': path})
+
+    return (img, graphics_new)
+    ## end of transformMapGroup()
+
+def plotMapGroup(map_group, ax):
+    _, graphics = map_group
+    n_col = len(plt.rcParams['axes.prop_cycle'])
+    print("Processing graphics:", len(graphics))
+    for i in range(len(graphics)):
+        col = "C" + str(i%n_col)
+        if graphics[i]['path']['colour'] is not None:
+            col = graphics[i]['path']['colour']
+        for curve in graphics[i]['path']['contour']:
             _ = curve.plot(num_pts = 256, color = col, ax = ax)
         # plot centroid i
-        # cx, cy, _ = np.matmul(np.append(graphics[i]['path']['centroid'], 1), m)
-        # ax.plot(cx, cy, "o")
+        cx, cy = graphics[i]['path']['centroid']
+        ax.plot(cx, cy, "o")
 
     ## end of plotMapGroup()
+
+def getAlertMasks(map_group):
+    _, graphics = map_group
+
+    # mask will have shape defined by the image map extent divided
+    # into 0.1 degree grid
+    res = 0.1
+    # lon_min, lon_max, lat_min, lat_max = [round(x, 1) for x in extent_MAP_IMG]
+    # nx, ny = np.array([lon_max - lon_min, lat_max - lat_min])/res
+    # img_shape = (round(nx), round(ny), 3)
+    lon_min, lon_max, lat_min, lat_max = extent_MAP_IMG
+    x = np.arange(lon_min, lon_max, res) # [round(x,1) for x in x]
+    y = np.arange(lat_min, lat_max, res) # [round(y,1) for y in y]
+    xx, yy = np.meshgrid(x, y)
+    xy = np.vstack((xx.ravel(), yy.ravel())).T
+
+    # Create transformation matrix
+    tm = np.array([res, 0, 0, res, lon_min, lat_min])
+    m = np.hstack((tm.reshape(3,2), np.array([[0],[0],[1]])))
+    try:
+        m_inv = LA.inv(m)
+    except LinAlgError:
+        sys.exit("Could not invert transformation matrix")
+
+    mask_list = []
+    for i in range(len(graphics)):
+        col = graphics[i]['path']['colour']
+        if col is not None and col.count(col[0]) != 3:
+            # got a contour with RGB colour
+            print(col)
+            alert_val = 0
+            r, g, b = col
+            if col == (0.0, 0.0, 0.0):
+                print("colour: black")
+            elif col == (1.0, 1.0, 0.0):
+                print("colour: yellow")
+                alert_val = 1
+            elif g > 0.33 and g < 0.66:
+                # (0.89, 0.424, 0.0392)
+                # (0.969, 0.588, 0.275)
+                print("colour: orange")
+                alert_val = 2
+            elif g < 0.33:
+                print("colour: red")
+                alert_val = 3
+            else:
+                print("colour: other")
+
+            #img = np.zeros(img_shape, dtype = np.double)
+            img2 = np.array([alert_val]*xx.size).reshape(xx.shape)
+            img = np.zeros(xx.shape, dtype = np.double)
+
+            # nodes_new = []
+            # for curve in graphics[i]['path']['contour']:
+            #     # transform curve to grid coords
+            #     nodes = curve.nodes
+            #     for i in range(len(nodes.T)):
+            #         # Multiply node by transformation matrix m to
+            #         # get grid coordinates
+            #         v = np.matmul(np.append(nodes.T[i], 1), m_inv)
+            #         nodes_new.append(v[:-1])
+            # nodes = np.array([node.round() for node in nodes_new])
+            # mask = skimage.draw.polygon2mask(img_shape[:-1], nodes)
+            # img[mask] = col
+            #mask_list.append(img)
+
+            ## alternative approach
+            vv = np.vstack([curve.nodes.T for curve in graphics[i]['path']['contour']])
+            # construct a Path from the vertices
+            pth = Path(vv, closed=False)
+
+            # test which pixels fall within the path
+            mask = pth.contains_points(xy)
+
+            # reshape to the same size as the grid
+            mask = mask.reshape(xx.shape)
+
+            # create a masked array
+            masked = np.ma.masked_array(img2, ~mask)
+
+            # or simply set values for masked pixels
+            img[mask] = alert_val
+
+            # combine with coords...
+            am = np.stack((xx, yy, img))
+
+            mask_list.append(am)
+    return mask_list
+    ## end
+
+def createGriddedData(map_groups, alert_data):
+    # container for gridded data layers
+    vars = {}
+
+    # data will have shape defined by the image map extent divided
+    # into 0.1 degree grid
+    res = 0.1
+    lon_min, lon_max, lat_min, lat_max = extent_MAP_IMG
+    x = np.arange(lon_min, lon_max, res) # [round(x,1) for x in x]
+    y = np.arange(lat_min, lat_max, res) # [round(y,1) for y in y]
+    xx, yy = np.meshgrid(x, y)
+    xy = np.vstack((xx.ravel(), yy.ravel())).T
+
+    # Create transformation matrix
+    tm = np.array([res, 0, 0, res, lon_min, lat_min])
+    m = np.hstack((tm.reshape(3,2), np.array([[0],[0],[1]])))
+    try:
+        m_inv = LA.inv(m)
+    except LinAlgError:
+        sys.exit("Could not invert transformation matrix")
+
+    for i, mg in enumerate(map_groups):
+        _, graphics = mg
+        print(i)
+
+        # count arrays added for this group
+        n = 0
+
+        for j, gfx in enumerate(graphics):
+            col = gfx['path']['colour']
+            #breakpoint()
+            if col is not None and col.count(col[0]) != 3:
+                # got a contour with RGB colour
+                print(col)
+                alert_val = 0
+                r, g, b = col
+                if col == (0.0, 0.0, 0.0):
+                    print("colour: black")
+                elif col == (1.0, 1.0, 0.0):
+                    print("colour: yellow")
+                    alert_val = 1
+                elif g > 0.33 and g < 0.66:
+                    # (0.89, 0.424, 0.0392)
+                    # (0.969, 0.588, 0.275)
+                    print("colour: orange")
+                    alert_val = 2
+                elif g < 0.33:
+                    print("colour: red")
+                    alert_val = 3
+                else:
+                    print("colour: other")
+
+                img = np.zeros(xx.shape, dtype = np.double)
+
+                # get nodes for the alert area
+                vv = np.vstack([curve.nodes.T for curve in gfx['path']['contour']])
+
+                # construct a Path from the vertices
+                pth = Path(vv, closed=False)
+
+                # test which pixels fall within the path
+                mask = pth.contains_points(xy)
+
+                # reshape to the same size as the grid
+                mask = mask.reshape(xx.shape)
+
+                # set values for masked pixels
+                img[mask] = alert_val
+
+                da = xr.DataArray(data=img, dims=["lat", "lon"], coords=[y, x])
+
+                da.attrs = {
+                    'issue_date': alert_data.loc[i,'issue_date'],
+                    'alert_date': alert_data.loc[i,'date'],
+                    'alert_day': alert_data.loc[i,'day'],
+                    'alert_weekday': alert_data.loc[i,'weekday'],
+                    'alert_id': n+1,
+                    'alert_type': '',
+                    'alert_text': alert_data.loc[i,'alert_text'],
+                }
+
+                var_name = '_'.join(['alert', 'day'+str(da.attrs['alert_day']),
+                                     str(da.attrs['alert_id'])])
+                vars[var_name] = da
+                n += 1
+
+    # combine data arrays into data set
+    issue_date = alert_data.loc[0, 'issue_date']
+    ds = xr.Dataset(data_vars=vars,
+                    attrs={
+                        'title': 'TMA weather warnings for ' + issue_date,
+                        'issue_date': issue_date,
+                    })
+    ds.to_netcdf('TMA_weather_warning_'+issue_date+'.nc')
+    ## end
 
 # Main
 def main():
 
     parser = argparse.ArgumentParser(description='Process TMA PDF contents')
     parser.add_argument('filepath', nargs=1, type=str)
+    parser.add_argument('metadata', nargs=1, type=str)
     args = parser.parse_args()
 
     try:
         # read lines from input file
         lines = readFile(args.filepath[0])
-    except: 
+    except:
         # IOError
         print("Input file not found:", args.filepath[0])
         sys.exit(4)
@@ -394,29 +612,44 @@ def main():
     print(len(images))
 
     mgroups = getMapGroups(images, graphics)
+    mgroups = [transformMapGroup(mg) for mg in mgroups]
 
-    # Downloaded from https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_TZA_shp.zip
-    shp_fname = '../../mapping/gadm/shapefile/gadm36_TZA_1.shp'
-    adm1_shapes = list(shpreader.Reader(shp_fname).geometries())
+    # Get associated data - one row per forecast date
+    alert_data = pd.read_csv(args.metadata[0])
 
-    #createPlot(images, graphics)
-    fig, axs = plt.subplots(1, 4, subplot_kw={'projection': ccrs.PlateCarree()})
-    for mg, ax in zip(mgroups, axs.flat):
+    createGriddedData(mgroups, alert_data)
 
-        ax.set_extent(extent_MAP_IMG, ccrs.PlateCarree())
-        ax.coastlines(resolution='10m')
-        ax.add_feature(cfeature.LAND)
-        ax.add_feature(cfeature.LAKES, alpha=0.5)
-        ax.add_geometries(adm1_shapes, ccrs.PlateCarree(),
-                          edgecolor='black', facecolor='gray', alpha=0.5)
-        arr_img = plt.imread(MAP_IMG, format='png')
-        ax.imshow(arr_img, interpolation='none',
-                  origin='upper',
-                  extent=extent_MAP_IMG,
-                  clip_on=True)
+    # am = getAlertMasks(mgroups[0])
+    # #fig, axs = plt.subplots(1, len(am))
+    # fig, ax = plt.subplots()
+    # #for m, ax in zip(am, axs.flat):
+    # for m in am:
+    #     print(m.shape)
+    #     ax.imshow(m, origin = 'lower', alpha=.3, vmin=0, vmax=1)
+    # plt.show()
 
-        plotMapGroup(mg, ax)
-    plt.show()
+    # # Downloaded from https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_TZA_shp.zip
+    # shp_fname = '../../mapping/gadm/shapefile/gadm36_TZA_1.shp'
+    # adm1_shapes = list(shpreader.Reader(shp_fname).geometries())
+
+    # #createPlot(images, graphics)
+    # fig, axs = plt.subplots(1, 4, subplot_kw={'projection': ccrs.PlateCarree()})
+    # for mg, ax in zip(mgroups, axs.flat):
+
+    #     ax.set_extent(extent_MAP_IMG, ccrs.PlateCarree())
+    #     ax.coastlines(resolution='10m')
+    #     ax.add_feature(cfeature.LAND)
+    #     ax.add_feature(cfeature.LAKES, alpha=0.5)
+    #     ax.add_geometries(adm1_shapes, ccrs.PlateCarree(),
+    #                       edgecolor='black', facecolor='gray', alpha=0.5)
+    #     arr_img = plt.imread(MAP_IMG, format='png')
+    #     ax.imshow(arr_img, interpolation='none',
+    #               origin='upper',
+    #               extent=extent_MAP_IMG,
+    #               clip_on=True)
+
+    #     plotMapGroup(mg, ax)
+    # plt.show()
     ## end of main()
 
 # Run main
